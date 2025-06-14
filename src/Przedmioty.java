51811a0b7c02381f40215f3884f7dfaf8d95fd89
student <student@ur.edu.pl>
2025-06-14 13:51:41 +0200

    przedmioty

---
+++ src/Przedmioty.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Przedmioty {
+    public static final List<String> lista = Arrays.asList(
+            "Matematyka",
+            "Język polski",
+            "Fizyka",
+            "Historia",
+            "Informatyka"
+    );
+
+    public static void wyswietlPrzedmioty() {
+        System.out.println("Dostępne przedmioty:");
+        int i = 1;
+        for (String przedmiot : lista) {
+            System.out.println(i + ". " + przedmiot);
+            i++;
+        }
+    }
+}
--- src/WyswietlUczniow.java
+++ src/WyswietlUczniow.java
@@ -1,20 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class WyswietlUczniow{
     public static void menuUczniowie() {
-        System.out.print("=-=Menu wyswietlania uczniow=-=");
-        System.out.print("Lista przedmiotów");
-        System.out.print("Lista wszystkich uczniow");
-        System.out.print("Wyswietl oceny wybranego ucznia");
-        System.out.print("wyswietl uczniow z konkretnego przedmiotu");
+        System.out.print("\n---------------------------------------------");
+        System.out.print("\n=-=Menu wyswietlania uczniow=-=");
+        System.out.print("\n1.Lista przedmiotów");
+        System.out.print("\n2.Lista wszystkich uczniow");
+        System.out.print("\n3.Wyswietl oceny wybranego ucznia");
+        System.out.print("\n5.Powrot do glownego Menu");
+        System.out.print("\n---------------------------------------------");
+
+        System.out.print("\nWybierz opcje: ");
         Scanner scanner = new Scanner(System.in);
         int wybor = scanner.nextInt();
         switch(wybor){
             case 1:
+            Przedmioty.wyswietlPrzedmioty();
                 break;
             case 2:
                 break;
+            case 5:
+                Menu.pokazmenu();
+                break;
             default:
                 break;
+
         }
     }
 
